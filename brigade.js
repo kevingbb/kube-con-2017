f8ad512bcc3bc08ceb61157bf917acd58c7716d3
Kevin Harris <Kevin@Kevins-MacBook-Pro.local>
2018-03-08 21:47:55 -0500

    Added some checking.

--- brigade.js
+++ brigade.js
@@ -1,6 +1,7 @@
 const { events, Job, Group } = require('brigadier')
 
 events.on("push", (brigadeEvent, project) => {
+    console.log("received push for commit " + e.commit)
     
     // setup variables
     var gitPayload = JSON.parse(brigadeEvent.payload)
